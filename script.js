753a5be9c8993c3dc3d713c775414b63d4c9bd26
straffdav <89692546+straffdav@users.noreply.github.com>
2023-04-19 03:06:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const deck = [];
+for (let i = 1; i <= 50; i++) {
+  deck.push(`card-${i}.png`);
+}
+
+shuffle(deck);
+
+const deckElem = document.querySelector(".deck");
+const topSquareElem = document.querySelector(".top-square");
+const bottomSquareElem = document.querySelector(".bottom-square");
+
+deckElem.addEventListener("click", () => {
+  if (deck.length > 0) {
+    const card = deck.pop();
+    deckElem.style.backgroundImage = `url(${card})`;
+    if (card === "card-50.png") {
+      alert("Game over!");
+    }
+  }
+});
+
+topSquareElem.addEventListener("click", () => {
+  if (deck.length > 0) {
+    const card = deck.pop();
+    topSquareElem.style.backgroundImage = `url(${card})`;
+    if (card === "card-50.png") {
+      alert("Game over!");
+    }
+  }
+});
+
+bottomSquareElem.addEventListener("click", () => {
+  if (deck.length > 0) {
+    const card = deck.pop();
+    bottomSquareElem.style.backgroundImage = `url(${card})`;
+    if (card === "card-50.png") {
+      alert("Game over!");
+    }
+  }
+});
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  
+  // Swap the last card with a random card to avoid always having the same final card
+  const lastIndex = array.length - 1;
+  const randomIndex = Math.floor(Math.random() * lastIndex);
+  [array[lastIndex], array[randomIndex]] = [array[randomIndex], array[lastIndex]];
+  
+  return array;
+}
